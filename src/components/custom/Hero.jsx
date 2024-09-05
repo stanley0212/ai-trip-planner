6d7ba886f1e0ca3479467f226f108905bcab1fd0
stanley0212 <trip7.net@gmail.com>
2024-09-05 21:50:03 +1000

    update

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
-      <h1 className=' font-extrabold text-[60px] text-center mt-16'><span className='text-[#f56551]'>Discover Your Next Adventure with</span> your families or friends</h1>
+      <h1 className=' font-extrabold text-[60px] text-center mt-16'><span className='text-[#f56551]'>Discover Your Next Adventure with AI: </span> You're a travel planner.</h1>
       <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
 
       <Link to={'/create-trip'}>
